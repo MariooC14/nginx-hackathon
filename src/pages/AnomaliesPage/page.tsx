028a00c22a10be5c02542565d4de730d0a64c2f4
Mario C <mario.caval@icloud.com>
2025-05-17 23:42:09 +0100

    Added geo location to anomaly details view

--- src/pages/AnomaliesPage/page.tsx
+++ src/pages/AnomaliesPage/page.tsx
@@ -3,12 +3,9 @@ import { AnomaliesDataTable } from "./data-table";
 import { columns as anomaliesColumns } from "./columns";
 import { useEffect, useState } from "react";
 import { anomalyService, type Anomaly } from "@/services/AnomalyService";
-import GeoMap from "@/components/geoMap.tsx";
-import { IPtoLocation, type LocationData } from "@/services/gpsService.ts";
 
 export default function AnomaliesPage() {
   const [anomalyLogs, setAnomalyLogs] = useState<Anomaly[]>([]);
-  const [locations, setLocations] = useState<LocationData[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -17,18 +14,12 @@ export default function AnomaliesPage() {
     setLoading(false);
   }, []);
 
-  useEffect(() => {
-    IPtoLocation("43.130.40.120", "185.198.228.124")
-      .then(newLocations => setLocations(newLocations));
-  }, []);
-
   return (
     <>
       <TypographyH1 className="inline">Anomalies</TypographyH1>
       <div>
         <AnomaliesDataTable columns={anomaliesColumns} data={anomalyLogs} loading={loading} />
       </div>
-      <GeoMap locations={locations}></GeoMap>
     </>
   );
 }
