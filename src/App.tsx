7b038f7daccd91062f8c6686b4be24d31239d81c
David Wilson <szabol.david04@gmail.com>
2025-05-17 12:59:53 +0100

    Netstat and Network log service

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import Layout from "./components/layout";
-import { Outlet } from "react-router";
+import NetworkLogGrid from "./components/NetworkLogGrid";
+import NetworkChartsView from "./components/NetworkChartsView";
+import { useNetworkLogs } from "./NetworkLogsProvider";
+import { useEffect } from "react";
+import DataTable from "./components/data-table";
+import { getUniqueVisitors, getTopPaths, getTotalRequests, getTotalSize } from "./services/netstats";
 
 function App() {
+  const logs = useNetworkLogs();
+
+  useEffect(() => {
+    console.log("Network logs:", logs);
+    console.log("Unique visitors", getUniqueVisitors());
+    console.log("Total size ", getTotalSize());
+    console.log("Total requests ", getTotalRequests());
+    console.log("Top paths ", getTopPaths());
+  }, [logs]);
 
   return (
     <Layout>
