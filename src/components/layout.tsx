22224decef1f1cc7abef3a851500f2b2551314c7
Mario C <mario.caval@icloud.com>
2025-05-17 20:54:24 +0100

    updated network logs service

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,3 +1,4 @@
+import NetworkLogsProvider from "@/NetworkLogsProvider";
 import { AppSidebar } from "./app-sidebar";
 import { ThemeProvider } from "./theme-provider";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "./ui/sidebar";
@@ -6,15 +7,17 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <SidebarProvider defaultOpen={true} className="flex max-w-screen">
-        <AppSidebar />
-        <SidebarInset>
-          <main className="p-4">
-            <SidebarTrigger />
-            {children}
-          </main>
-        </SidebarInset>
-      </SidebarProvider>
+      <NetworkLogsProvider>
+        <SidebarProvider defaultOpen={true} className="flex max-w-screen">
+          <AppSidebar />
+          <SidebarInset>
+            <main className="p-4">
+              <SidebarTrigger />
+              {children}
+            </main>
+          </SidebarInset>
+        </SidebarProvider>
+      </NetworkLogsProvider>
     </ThemeProvider>
   )
 }
