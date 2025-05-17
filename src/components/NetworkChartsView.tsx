6c890f04e45f86e8d2b36f2159fcf069af7028e7
Celso Vinicius <celsovinicis.f@gmail.com>
2025-05-17 19:24:53 +0100

    Minor change to dashboard ui divs

--- src/components/NetworkChartsView.tsx
+++ src/components/NetworkChartsView.tsx
@@ -1,18 +1,23 @@
-import DataServedChart from "./charts/DataServedChart";
 import NetworkTrafficLineChart from "./charts/NetworkTrafficLineChart";
 import StatusPieChart from "./charts/StatusPieChart";
 import UniqueVisitorsChart from "./charts/UniqueVisitorsChart";
+import DataServedChart from "./charts/DataServedChart";
 
 export default function NetworkChartsView() {
-
   return (
     <div className="m-8">
-      <br />
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-        <NetworkTrafficLineChart />
-        <UniqueVisitorsChart />
-        <DataServedChart />
         <StatusPieChart />
+        <StatusPieChart />
+      </div>
+
+      <div className="p-4 max-w">
+        <NetworkTrafficLineChart />
+      </div>
+
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+          <UniqueVisitorsChart />
+          <DataServedChart />
       </div>
     </div>
   )
