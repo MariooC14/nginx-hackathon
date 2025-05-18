89862a9d9f71f8afa16d09840cc9ee8f58114dfe
Celso Vinicius Franco Ferreira <celsovinicius_f@hotmail.com>
2025-05-18 10:03:58 +0100

    Added total data served card

--- src/components/charts/SectionCards.tsx
+++ src/components/charts/SectionCards.tsx
@@ -6,13 +6,14 @@ import {
 } from "@/components/ui/card"
 import { anomalyService, type Anomaly } from "@/services/AnomalyService";
 
-import { getUniqueVisitors, getTotalRequests } from "@/services/netstats"
+import { getUniqueVisitors, getTotalRequests, getTotalSize } from "@/services/netstats"
 import { useState, useEffect } from "react"
 
 export function SectionCards() {
     const [uniqueVisitors, setUniqueVisitors] = useState<number | null>(null);
     const [totalRequests, setTotalRequests] = useState<number | null>(null);
     const [anomalyCount, setAnomalyCount] = useState<number | null>(null);
+    const [dataServed, setDataServed] = useState<number | null>(null);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -20,12 +21,13 @@ export function SectionCards() {
             setAnomalyCount(anomalies.length);
             setUniqueVisitors(getUniqueVisitors());
             setTotalRequests(await getTotalRequests());
+            setDataServed(await getTotalSize());
         };
         fetchData();
     }, [])
 
   return (
-    <div className="grid grid-cols-3 col-span-3 row-span-2  gap-4 *:data-[slot=card]:shadow-xs *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card">
+    <div className="grid grid-cols-4 col-span-4 row-span-2  gap-4 *:data-[slot=card]:shadow-xs *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card">
       <Card className="@container/card">
         <CardHeader className="flex flex-col items-center justify-center flex-1 relative text-center">
           <CardDescription>Unique Visitors</CardDescription>
@@ -49,6 +51,15 @@ export function SectionCards() {
           <CardDescription>Anomalies Found</CardDescription>
           <CardTitle className="text-center @[250px]/card:text-3xl @[400px]/card:text-4xl text-2xl font-semibold tabular-nums">
             {anomalyCount !== null ? anomalyCount : "Loading..."}
+          </CardTitle>
+        </CardHeader>
+      </Card>
+
+      <Card className="@container/card">
+        <CardHeader className="flex flex-col items-center justify-center flex-1 relative text-center">
+          <CardDescription>Total Data Served</CardDescription>
+          <CardTitle className="text-center @[250px]/card:text-3xl @[400px]/card:text-4xl text-2xl font-semibold tabular-nums">
+            {dataServed !== null ? `${(dataServed / (1024 * 1024 * 1024)).toFixed(2)} GB` : "Loading..."}
           </CardTitle>
         </CardHeader>
       </Card>
